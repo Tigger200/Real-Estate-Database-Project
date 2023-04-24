6265f0b0971839ebaa9d88a14cd7be7caa30dfa0
mary fuller <marymefuller@gmail.com>
2023-04-24 15:14:05 -0400

    added property code

---
+++ seeders/20230424185931-real-estate-properties.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { BLOB } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Properties',[{
+      address: "101 Summit Ave, West Seneca, NY 14224",
+      client: 'Bob', 
+      agent_id:2,
+      createdAt: new Date(),
+      UpdatedAt: new Date()
+    }])
+  },
+
+
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
